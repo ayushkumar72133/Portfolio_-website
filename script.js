c27a24a44f8b8590b0630abb584cff1759639079
ayushkumar72133 <122511203+ayushkumar72133@users.noreply.github.com>
2024-02-09 14:29:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents= document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname){
+//   for(tablink of tablinks){
+//     tablink.class.List.remove("active-link");
+//   }
+//   for(tablink of tabcontents){
+//     tabcontent.class.List.remove("activ-tab");
+//   }
+//   event.currentTarget.classList.add("active-link");
+//   document.getElementById(tabname).classList.add("active-tab");
+// }
+
+
+
+// typing text animation script for home page
+var typed = new Typed(".typing-2", {
+  strings: ["Ayush"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+
